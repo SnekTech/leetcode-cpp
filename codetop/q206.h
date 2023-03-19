4a36c0a8e4feefcf364b750dead0599152dfd5c3
syf <2654525303@qq.com>
2023-03-20 00:35:14 +0800

    ✔ CodeTop q206

---
+++ codetop/q206.h
@@ -0,0 +1,27 @@
+//
+// Created by syf on 2023/3/20.
+//
+
+#ifndef LEETCODE_CPP_Q206_H
+#define LEETCODE_CPP_Q206_H
+
+#include "ListNode.h"
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode dummy(-1);
+        auto p = head;
+        while (p != nullptr)
+        {
+            auto nextP = p->next;
+            p->next = dummy.next;
+            dummy.next = p;
+            p = nextP;
+        }
+
+        return dummy.next;
+    }
+};
+
+#endif //LEETCODE_CPP_Q206_H
