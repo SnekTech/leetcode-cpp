9fceee04d07acd4e4e8fdd1bc04cb95909777eb4
syf <2654525303@qq.com>
2023-04-05 23:39:05 +0800

    ✔ CodeTop q322

---
+++ codetop/q322.h
@@ -0,0 +1,25 @@
+//
+// Created by syf on 2023/4/5.
+//
+
+#pragma once
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int Max = amount + 1;
+        vector<int> dp(amount + 1, Max);
+
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++)
+            for (int j = 0; j < coins.size(); j++)
+                if (coins[j] <= i)
+                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+};
