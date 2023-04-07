2c99fd74e6ee785adb5b92ab229f8cde528404a0
syf <2654525303@qq.com>
2023-04-07 23:35:49 +0800

    ✔ q300

---
+++ codetop/q300.h
@@ -0,0 +1,29 @@
+//
+// Created by syf on 2023/4/7.
+//
+#pragma once
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1);
+
+        int maxLength = 1;
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (nums[j] < nums[i])
+                    dp[i] = max(dp[i], dp[j] + 1);
+            }
+            maxLength = max(maxLength, dp[i]);
+        }
+
+        return maxLength;
+    }
+};
