746c37ae127dd4a5381448af1753396fe3b8ae77
syf <2654525303@qq.com>
2023-04-16 22:24:31 +0800

    ✔ q105 - optimize

--- codetop/q105.h
+++ codetop/q105.h
@@ -2,15 +2,24 @@
 // Created by syf on 2023/4/16.
 //
 #pragma once
+
 #include <vector>
+#include <unordered_map>
 #include "Common.h"
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+    unordered_map<int, int> cache;
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder)
+    {
         int n = (int)preorder.size();
+        for (int i = 0; i < n; i++)
+            cache[inorder[i]] = i;
+
         return build(preorder, 0, n - 1, inorder, 0, n - 1);
     }
 
@@ -20,18 +29,8 @@ public:
             return nullptr;
 
         auto root = new TreeNode(pre[preLeft]);
-        if (preLeft == preRight)
-        {
-            return root;
-        }
 
-        int rootIndex = -1;
-        for (int i = inLeft; i <= inRight; i++)
-            if (in[i] == root->val)
-            {
-                rootIndex = i;
-                break;
-            }
+        int rootIndex = cache[root->val];
 
         int leftChildLength = rootIndex - inLeft;
         root->left = build(pre, preLeft + 1, preLeft + leftChildLength, in, inLeft, rootIndex - 1);
