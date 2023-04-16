d711a974d9cd472b642b94c22c6a87e7fc3c1a3a
syf <2654525303@qq.com>
2023-04-16 19:17:20 +0800

    quick sort - two way

--- sorting/quick-sort.cpp
+++ sorting/quick-sort.cpp
@@ -26,12 +26,37 @@ static int Partition(vector<int>& nums, int left, int right)
     return j;
 }
 
+static int Partition2(vector<int>& nums, int left, int right)
+{
+    const int v = nums[left];
+    int i = left + 1;
+    int j = right;
+
+    while (true)
+    {
+        while (i <= right && nums[i] <= v)
+            i++;
+        while (j >= left + 1 && nums[j] >= v)
+            j++;
+
+        if (i > j)
+            break;
+
+        swap(nums[i], nums[j]);
+        i++;
+        j--;
+    }
+
+    swap(nums[left], nums[j]);
+    return j;
+}
+
 static void Quick(vector<int>& nums, int left, int right)
 {
     if (left >= right)
         return;
 
-    int pivot = Partition(nums, left, right);
+    int pivot = Partition2(nums, left, right);
     Quick(nums, left, pivot - 1);
     Quick(nums, pivot + 1, right);
 }
